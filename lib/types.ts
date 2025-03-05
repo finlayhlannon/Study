a5b3b8636d443a444c131faed2b4dd695447b012
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2025-03-04 21:13:51 -0800

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,22 +1,22 @@
-// lib/types.ts
-export interface Topic {
-  id: string
-  name: string
-  description?: string
-  notes?: string
-  knowledgeLevel: number
-  dueDate: string
+export interface Unit {
+  id: string;
+  name: string;
+  courseId: string;
+  topics: Topic[];
 }
 
-export interface Unit {
-  id: string
-  name: string
-  number: number
-  topics: Topic[]
+export interface Topic {
+  id: string;
+  name: string;
+  unitId: string;
+  description?: string;
+  dueDate: string;
+  knowledgeLevel: number;
+  notes?: string;
 }
 
 export interface Course {
-  id: string
-  name: string
-  units: Unit[]
+  id: string;
+  name: string;
+  units: Unit[];
 }
