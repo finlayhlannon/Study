b2154ea2cedee99eb61fdc1b12d09b4d204ecf68
Finlay Hlannon <79950262+finlayhlannon@users.noreply.github.com>
2025-03-04 19:29:33 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Study',
+  description: 'Created by Finlay Hlannon',
   generator: 'v0.dev',
 }
 
